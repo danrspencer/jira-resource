17bc4a180c5786544d674803caa40917cabb3505
Spencer, Daniel <Daniel.Spencer2@bskyb.com>
2016-06-13 09:11:50 +0100

    Re-implementation of resource in node

--- spec/outSpec.js
+++ spec/outSpec.js
@@ -1,35 +1,463 @@
 
+const chai = require('chai');
+const expect = chai.expect;
 
+const moment = require('moment');
 const nock = require('nock');
+const out = require('../src/out.js');
+
+const jiraUrl = 'http://jira.com';
+const jiraUser = 'dave';
+const jiraPass = 'letmein';
 
 nock.disableNetConnect();
 
-const out = require('../src/out.js');
-
 describe('prometheus client', () => {
 
-    const jiraUrl = 'http://jira.com';
+    beforeEach(() => {
+        nock.cleanAll();
+    });
 
-    it('returns the issue key', () => {
+    it('checks for an existing issue', (done) => {
+        let search = setupSearch();
 
-        nock(jiraUrl)
-            .get('/')
-            .reply(200, { issues: [ { key: 'TEST-123' } ] });
+        out(concourseInput(), () => {
+            expect(search.isDone()).to.be.true;
+            done();
+        });
+    });
 
+    describe('no existing issue', () => {
+        let create;
 
+        beforeEach(() => {
+            setupSearch();
 
+            create = setupCreate({
+                    fields: {
+                        project: {
+                            key: "ATP"
+                        },
+                        issuetype: {
+                            name: "Bug"
+                        },
+                        summary: "TEST 1.106.0",
+                        description: "Inline static description"
+                    }
+                });
+        });
 
-        var result = out({
-                params: {},
-                source: {
-                    url: jiraUrl
+        it('creates the jira issue', (done) => {
+            out(concourseInput(), () => {
+                expect(create.isDone()).to.be.true;
+                done();
+            });
+        });
+
+        it('returns the issue key', (done) => {
+            out(concourseInput(), (result) => {
+                expect(result).to.deep.equal({
+                    version: {
+                        issue: "ATP-1"
+                    }
+                });
+                done();
+            });
+        });
+
+        it('handles an additional field', (done) => {
+            let input = concourseInput();
+
+            input.params.environment = 'PROD';
+
+            let createWithEnv = setupCreate({
+                    fields: {
+                        project: {
+                            key: "ATP"
+                        },
+                        issuetype: {
+                            name: "Bug"
+                        },
+                        summary: "TEST 1.106.0",
+                        description: "Inline static description",
+                        environment: "PROD"
+                    }
+                });
+
+            out(input, () => {
+                expect(createWithEnv.isDone()).to.be.true;
+                done();
+            });
+        });
+
+        it('parses a $NOW', (done) => {
+            let input = concourseInput();
+
+            input.params.duedate = '$NOW';
+
+            let createWithDate = setupCreate((body) => {
+                    let duedate = moment(body.fields.duedate);
+                    let now = moment();
+
+                    return Math.abs(duedate.diff(now, 's')) < 1;
+                });
+
+            out(input, () => {
+                expect(createWithDate.isDone()).to.be.true;
+                done();
+            });
+        });
+
+        it('parses a $NOWs with +/-', (done) => {
+            let input = concourseInput();
+
+            input.params.duedate = '$NOW+5';
+            input.params.tomorrow = '$NOW+1d';
+            input.params.abitago = '$NOW-8h'
+
+            let createWithDate = setupCreate((body) => {
+                let duedate = moment(body.fields.duedate);
+
+                let expectedDueDate = moment().add(5, 'm');
+
+                return Math.abs(expectedDueDate.diff(duedate, 's')) < 1;
+            });
+
+            out(input, () => {
+                expect(createWithDate.isDone()).to.be.true;
+                done();
+            });
+        });
+
+        it('parses a $NOWs with +/- and units', (done) => {
+            let input = concourseInput();
+
+            input.params.tomorrow = '$NOW+1d';
+            input.params.abitago = '$NOW-8h'
+
+            let createWithDate = setupCreate((body) => {
+                    let tomorrow = moment(body.fields.tomorrow);
+                    let abitago = moment(body.fields.abitago);
+
+                    let expectedTomorrow = moment().add(1, 'd');
+                    let expectedAbitago = moment().add(-8, 'h');
+
+                    return Math.abs(expectedTomorrow.diff(tomorrow, 's')) < 1
+                        && Math.abs(expectedAbitago.diff(abitago, 's')) < 1;
+                });
+
+            out(input, () => {
+                expect(createWithDate.isDone()).to.be.true;
+                done();
+            });
+        });
+
+        it('handles a custom field', (done) => {
+             let input = concourseInput();
+
+            input.params.custom_fields = {
+                something: {
+                    id: 10201,
+                    value: 'dave!'
                 }
-            }
-        );
+            };
 
-        expect(result).toEqual({
-            version: { issue: 'TEST-123' }
+            let createWithCustom = setupCreate({
+                fields: {
+                    project: {
+                        key: "ATP"
+                    },
+                    issuetype: {
+                        name: "Bug"
+                    },
+                    summary: "TEST 1.106.0",
+                    description: "Inline static description",
+                    customfield_10201: 'dave!'
+                }
+            });
+
+            out(input, () => {
+                expect(createWithCustom.isDone()).to.be.true;
+                done();
+            });
+        });
+
+        it('handles multiple custom fields', (done) => {
+            let input = concourseInput();
+
+            input.params.custom_fields = {
+                something: {
+                    id: 10201,
+                    value: 'dave!'
+                },
+                somethingElse: {
+                    id: 12345,
+                    value: 'meh'
+                }
+            };
+
+            let createWithCustom = setupCreate({
+                fields: {
+                    project: {
+                        key: "ATP"
+                    },
+                    issuetype: {
+                        name: "Bug"
+                    },
+                    summary: "TEST 1.106.0",
+                    description: "Inline static description",
+                    customfield_10201: 'dave!',
+                    customfield_12345: 'meh'
+                }
+            });
+
+            out(input, () => {
+                expect(createWithCustom.isDone()).to.be.true;
+                done();
+            });
+        });
+    });
+
+    describe('existing issue', () => {
+        let update;
+
+        beforeEach(() => {
+            let issueId = 15805;
+
+            setupSearch([
+                {
+                    expand: "operations,versionedRepresentations,editmeta,changelog,renderedFields",
+                    id: issueId,
+                    self: "https://infinityworks.atlassian.net/rest/api/2/issue/" + issueId,
+                    key: "ATP-1",
+                    fields: {
+                        summary: "TEST 1.106.0"
+                    }
+                }
+            ]);
+
+            update = nock(jiraUrl)
+                .put('/rest/api/2/issue/' + issueId, {
+                    fields: {
+                        project: {
+                            key: "ATP"
+                        },
+                        issuetype: {
+                            name: "Bug"
+                        },
+                        summary: "TEST 1.106.0",
+                        description: "Inline static description"
+                    }
+                })
+                .basicAuth({
+                    user: jiraUser,
+                    pass: jiraPass
+                })
+                .reply(200)
+        });
+
+        it('updates the issue', (done) => {
+            out(concourseInput(), () => {
+                expect(update.isDone()).to.be.true;
+                done();
+            });
+        });
+
+        it('returns the issue key', (done) => {
+            out(concourseInput(), (result) => {
+                expect(result).to.deep.equal({
+                    version: {
+                        issue: "ATP-1"
+                    }
+                });
+                done();
+            });
+        });
+    });
+
+    describe('transitions', () => {
+
+        beforeEach(() => {
+            let issueId = 15805;
+
+            setupSearch([
+                {
+                    expand: "operations,versionedRepresentations,editmeta,changelog,renderedFields",
+                    id: issueId,
+                    self: "https://infinityworks.atlassian.net/rest/api/2/issue/" + issueId,
+                    key: "ATP-1",
+                    fields: {
+                        summary: "TEST 1.106.0"
+                    }
+                }
+            ]);
+
+            nock(jiraUrl)
+                .put('/rest/api/2/issue/' + issueId, {
+                    fields: {
+                        project: {
+                            key: "ATP"
+                        },
+                        issuetype: {
+                            name: "Bug"
+                        },
+                        summary: "TEST 1.106.0",
+                        description: "Inline static description"
+                    }
+                })
+                .basicAuth({
+                    user: jiraUser,
+                    pass: jiraPass
+                })
+                .reply(200)
+        });
+
+
+        it('updates an issue with a transition', (done) => {
+            let issueId = 15805;
+
+            nock(jiraUrl)
+                .get('/rest/api/2/issue/' + issueId + '/transitions/')
+                .basicAuth({
+                    user: jiraUser,
+                    pass: jiraPass
+                })
+                .reply(200, {
+                    "expand": "transitions",
+                    "transitions": [
+                        {
+                            "id": "51",
+                            "name": "Submit"
+                        }
+                    ]
+                });
+
+            let transition = nock(jiraUrl)
+                .post('/rest/api/2/issue/' + issueId + '/transitions/', {
+                    transition: {
+                        id: "51"
+                    }
+                })
+                .basicAuth({
+                    user: jiraUser,
+                    pass: jiraPass
+                })
+                .reply(204);
+
+            let input = concourseInput();
+
+            input.params.transitions = [
+                'Submit'
+            ];
+
+            out(input, () => {
+                expect(transition.isDone()).to.be.true;
+                done();
+            });
+        });
+
+        it('can handle multiple possible transitions', (done) => {
+            let issueId = 15805;
+
+            let input = concourseInput();
+            input.params.transitions = [
+                'Reject'
+            ];
+
+            nock(jiraUrl)
+                .get('/rest/api/2/issue/' + issueId + '/transitions/')
+                .basicAuth({
+                    user: jiraUser,
+                    pass: jiraPass
+                })
+                .reply(200, {
+                    "expand": "transitions",
+                    "transitions": [
+                        {
+                            "id": "51",
+                            "name": "Submit"
+                        },
+                        {
+                            "id": "60",
+                            "name": "Reject"
+                        }
+                    ]
+                });
+
+            let transition = nock(jiraUrl)
+                .post('/rest/api/2/issue/' + issueId + '/transitions/', {
+                    transition: {
+                        id: "60"
+                    }
+                })
+                .basicAuth({
+                    user: jiraUser,
+                    pass: jiraPass
+                })
+                .reply(204);
+
+
+
+            out(input, () => {
+                expect(transition.isDone()).to.be.true;
+                done();
+            });
         });
     });
 
 });
+
+function setupSearch(issues) {
+    issues = issues || [];
+
+    return nock(jiraUrl)
+        .post('/rest/api/2/search/', {
+            jql: 'project="ATP" AND summary~"TEST 1.106.0" ORDER BY id DESC',
+            maxResults: 1,
+            fields: [
+                "key",
+                "summary"
+            ]
+        })
+        .basicAuth({
+            user: jiraUser,
+            pass: jiraPass
+        })
+        .reply(200, {
+            expand: "names,schema",
+            startAt: 0,
+            maxResults: 1,
+            total: issues.length,
+            issues: issues
+        });
+}
+
+function setupCreate(expectedBody) {
+    return nock(jiraUrl)
+        .post('/rest/api/2/issue/', expectedBody)
+        .basicAuth({
+            user: jiraUser,
+            pass: jiraPass
+        })
+        .reply(200, {
+            id: "15805",
+            key: "ATP-1",
+            self: jiraUrl + "rest/api/2/issue/15805"
+        });
+}
+
+function concourseInput() {
+    return {
+        params: {
+            summary: "TEST 1.106.0",
+            description: "Inline static description",
+            issue_type: "Bug"
+        },
+        source: {
+            url: jiraUrl,
+            username: jiraUser,
+            password: jiraPass,
+            project: "ATP"
+        }
+    };
+}
--- src/out.js
+++ src/out.js
@@ -1,24 +1,197 @@
 
+const _ = require('lodash');
+const debug = require('debug')('jira-resource');
+const moment = require('moment');
 const request = require('request');
-const sleep   = require('sleep');
 
-module.exports = (input) => {
+/** Uncomment the below for useful debug shizzles **/
+//require('request-debug')(request);
 
+module.exports = (input, callback) => {
     const source = input.source;
     const params = input.params;
 
-    var issueKey;
+    debug('Searching for issue: %s', input.params.summary);
 
-    request.get(source.url, (error, response, body) => {
-        console.log(body);
+    searchBySummary(source, params, (error, response, body) => {
 
-        issueKey = body.issues[0].key;
+        debugResponse(response);
+
+        let issueExists = body.issues.length == 0;
+
+        if (issueExists) {
+            debug('Issue doesn\'t exist, creating new issue...');
+
+            createIssue(source, params, (error, response, body) => {
+                if (!body) {
+                    // TODO: Better error handling
+                    return callback();
+                }
+
+                debugResponse(response);
+
+                callback({
+                    version: {
+                        issue: body.key
+                    }
+                });
+            });
+        } else {
+            let issueId = body.issues[0].id;
+            let issueKey = body.issues[0].key;
+
+            debug('Issue exists [%s], updating issue...', issueKey);
+
+            updateIssue(issueId, source, params, (error, response, body) => {
+                debugResponse(response);
+
+                processTrasitions(issueId, source, params, () => {
+                    callback({
+                        version: {
+                            issue: issueKey
+                        }
+                    });
+                });
+            });
+        }
     });
+};
 
-    while (!issueKey) {
-        console.log(issueKey);
+function searchBySummary(source, params, callback) {
+    const searchUrl = source.url + '/rest/api/2/search/';
+    const jql = 'project="' + source.project + '" AND summary~"' + params.summary + '" ORDER BY id DESC';
 
+    let search = {
+        jql: jql,
+        maxResults: 1,
+        fields: [
+            "key",
+            "summary"
+        ]
+    };
+
+    debug('Sending search: %s', JSON.stringify(search, null, 2));
+
+    request({
+        method: 'POST',
+        uri: searchUrl,
+        auth: {
+            username: source.username,
+            password: source.password
+        },
+        json: search
+    }, callback);
+}
+
+function createIssue(source, params, callback) {
+    requestIssue(source.url + "/rest/api/2/issue/", 'POST', source, params, callback);
+}
+
+function updateIssue(issueId, source, params, callback) {
+    requestIssue(source.url + "/rest/api/2/issue/" + issueId, 'PUT', source, params, callback);
+}
+
+function requestIssue(issueUrl, method, source, params, callback) {
+
+    let issue = {
+        fields: {
+            project: {
+                key: source.project
+            },
+            issuetype: {
+                name: params.issue_type
+            }
+        }
+    };
+
+    params = parseCustomFields(params);
+
+    issue.fields = _(params).omit([ 'issue_type', 'custom_fields', 'transitions' ])
+        .mapValues(replaceNowString)
+        .merge(issue.fields);
+
+    debug('Sending issue: %s', JSON.stringify(issue, null, 2));
+
+    request({
+        method: method,
+        uri: issueUrl,
+        auth: {
+            username: source.username,
+            password: source.password
+        },
+        json: issue
+    }, callback);
+}
+
+function processTrasitions(issueId, source, params, callback) {
+    const transitionUrl = source.url + '/rest/api/2/issue/' + issueId + '/transitions/';
+
+    if (!params.transitions) {
+        callback();
     }
 
-    return issueKey;
-};
+    request({
+        method: 'GET',
+        uri: transitionUrl,
+        auth: {
+            username: source.username,
+            password: source.password
+        },
+        json: true
+    }, (error, response, body) => {
+        let transitionId = _.filter(body.transitions, (transition) => {
+            return transition.name == params.transitions[0]
+        })[0].id;
+
+        request({
+            method: 'POST',
+            uri: transitionUrl,
+            auth: {
+                username: source.username,
+                password: source.password
+            },
+            json: {
+                transition: {
+                    id: transitionId
+                }
+            }
+        }, callback);
+    });
+}
+
+function replaceNowString(value) {
+    return value.replace(/\$NOW([-+][0-9]+)?([ywdhms])?/, (match, change, unit) => {
+        let date = moment();
+
+        unit = unit || 'm';
+
+        if (change) {
+            date = date.add(change, unit);
+        }
+
+        return date.format();
+    });
+}
+
+function debugResponse(response, body) {
+    debug(
+        'Result: (%s) %s',
+        response.statusCode,
+        JSON.stringify(response.body || {}, null, 2)
+    );
+}
+
+function parseCustomFields(params) {
+    if (!params.custom_fields) {
+        return params;
+    }
+
+    return _(params.custom_fields)
+        .mapKeys((value) => {
+            return 'customfield_' + value.id
+        })
+        .mapValues((value) => {
+            return value.value
+        })
+        .merge(params);
+}
